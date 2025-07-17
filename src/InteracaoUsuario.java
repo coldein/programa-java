7c9904e9764342df03a788fbcfe60e0b0a9763f4
Victor <satovitor2730@gmail.com>
2025-07-17 17:09:24 -0400

    exercicio de interacao com usuario

---
+++ src/InteracaoUsuario.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class InteracaoUsuario {
+    public static void main(String[] args) {
+        int idade;
+
+        double altura = 0, peso = 0;
+
+        String nome = "", documento = "", estadoCivil = "";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Iniciando sistema....");
+        System.out.println("Seja Bem-vindo(a)");
+
+        do {
+            System.out.println("Digite seu nome completo: ");
+            nome = input.nextLine();
+            if (nome.trim().isEmpty()) {
+                System.out.println("Seu nome não pode estar vazio!\n");
+            }
+        } while (nome.trim().isEmpty());
+
+        do {
+            System.out.println("Digite seu CPF: ");
+            documento = input.nextLine();
+            if (documento.trim().isEmpty()) {
+                System.out.println("Seu CPF não pode estar vazio!");
+            }
+
+        } while (documento.trim().isEmpty());
+
+        do {
+            System.out.println("Digite seu estado civil: ");
+            estadoCivil = input.nextLine();
+            if (estadoCivil.trim().isEmpty()) {
+                System.out.println("Seu estado civil estar vazio!\n");
+            }
+
+        } while (estadoCivil.trim().isEmpty());
+
+        do {
+            System.out.println("Digite sua idade: ");
+            while (!input.hasNextInt()) {
+                System.out.println("Entrada inválida. Digite novamente:");
+                input.next();
+            }
+            idade = input.nextInt();
+            if (idade <= 0) {
+                System.out.println("O valor deve ser positivo.");
+            }
+
+        } while (idade <= 0);
+
+        do {
+            System.out.println("Digite seu peso: ");
+            while (!input.hasNextDouble()) {
+                System.out.println("Entrada inválida. Digite novamente:");
+                input.next();
+            }
+            peso = input.nextDouble();
+            if (peso <= 0) {
+                System.out.println("O valor deve ser positivo.");
+            }
+        } while (peso <= 0);
+
+        do {
+            System.out.println("Digite sua altura: ");
+            while (!input.hasNextDouble()) {
+                System.out.println("Entrada inválida. Digite novamente:");
+                input.next();
+            }
+            altura = input.nextDouble();
+            if (altura <= 0) {
+                System.out.println("O valor deve ser positivo.");
+            }
+        } while (altura <= 0);
+
+        input.close();
+    }
+}
