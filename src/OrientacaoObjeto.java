908f7362590ce723f117016e3d2a446fc9ee0ca5
Marcos Victor <satovitor2730@gmail.com>
2025-07-17 20:29:20 -0400

    inicia orientacao objeto

--- src/OrientacaoObjeto.java
+++ src/OrientacaoObjeto.java
@@ -1,2 +1,15 @@
 public class OrientacaoObjeto {
+    public static void main(String[] args) {
+
+        System.out.println(System.getenv("JAVA_HOME"));
+        OrientacaoObjeto.printMessage("Running");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
+    }
+
+    public static void printMessage(String mensagem) {
+        System.out.println(mensagem);
+    }
 }
---
+++ src/com/orientaca/objeto/entity/Pessoa.java
@@ -0,0 +1,18 @@
+package com.orientaca.objeto.entity;
+
+public class Pessoa {
+
+    String cpf;
+    byte[] foto;
+    String sexo;
+
+    //comportamentos -> métodos
+
+    // Plain Old Java Objects POJO
+
+    // classe -> entidade do sistema que possui atributos e metodos
+
+    public void consultarObjeto(String cpf){
+
+    }
+}
