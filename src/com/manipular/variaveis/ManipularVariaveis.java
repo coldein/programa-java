ccaef853e726c2277f69800a1dc561a1565b9211
Victor <satovitor2730@gmail.com>
2025-07-23 18:14:43 -0400

    Exercicio de manipualr variaveis

---
+++ src/com/manipular/variaveis/ManipularVariaveis.java
@@ -0,0 +1,9 @@
+package com.manipular.variaveis;
+
+public class ManipularVariaveis {
+    public static void main(String[] args) {
+        String string = "5";
+        int convertido = Integer.parseInt(string) + 10;
+        System.out.println(convertido);
+    }
+}
